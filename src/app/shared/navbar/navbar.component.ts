177036920f6062a14bd42c8fc4c8351dd4119959
Ariel Aguilar <arielaguilar1994@gmail.com>
2021-04-12 08:54:25 -0300

    Cambios en el navbar para reenderizar el nombre del usuario

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import { AppState } from 'src/app/app.reducer';
+import { Usuario } from 'src/app/models/usuario.model';
 
 @Component({
   selector: 'app-navbar',
@@ -6,11 +10,19 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  userSubcription: Subscription;
+  user: Usuario;
+
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {
+    this.userSubcription = this.store.select('auth').subscribe(({ user }) => this.user = user);
+  }
+
+  ngOnDestroy(): void {
+    this.userSubcription.unsubscribe();
   }
 
 }
